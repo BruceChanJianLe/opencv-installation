450dd126e978b8a8671f9a7c1315f226ea25bb40
CHAN JIAN LE <49647142+BruceChanJianLe@users.noreply.github.com>
2020-05-15 19:09:16 +0800

    Update install_opencv.sh

--- install_opencv.sh
+++ install_opencv.sh
@@ -2,6 +2,12 @@
 # This script will install OpenCV {CHOOSE_A_VERSION} on your Ubuntu {TESTED_WITH_16.04}
 
 read -p "Please provide the version of OpenCV you would like to install..." VERSION
+read -p "Please press 0 to select simple installation and 1 for complicated installation" INSTALLATION
+
+if [ -z $INSTALLATION ]
+then
+    INSTALLATION=0
+fi
 
 # Update your system
 sudo apt-get update -y
@@ -29,10 +35,13 @@ sudo python3 get-pip.py
 # Install numpy
 sudo pip install numpy -y
 
-# Clone OpenCV and OpenCV contrib
-git clone https://github.com/Itseez/opencv_contrib.git
-cd opencv_contrib
-git checkout ${VERSION}
+if [ $INSTALLATION -eq 1 ]
+then
+  # Clone OpenCV and OpenCV contrib
+  git clone https://github.com/Itseez/opencv_contrib.git
+  cd opencv_contrib
+  git checkout ${VERSION}
+fi
 
 cd ..
 git clone https://github.com/Itseez/opencv.git
@@ -42,7 +51,13 @@ git checkout ${VERSION}
 # Create build directory
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D BUILD_EXAMPLES=ON ..
+if [ $INSTALLATION -eq 1]
+then
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D BUILD_EXAMPLES=ON ..
+elif [ $INSTALLATION -eq 0 ]
+then
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_C_EXAMPLES=ON -D BUILD_EXAMPLES=ON ..
+fi
 
 # Compile with the number of thread your computer has
 make -j15
